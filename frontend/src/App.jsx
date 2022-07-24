33788ff55186bcd8a9143e465659b0c115a59b82
Jade <hendricksjade@gmail.com>
2022-07-24 16:59:04 +0200

    adding in the homescreen and product listings components.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from "./components/Footer";
+import HomeScreen from './screens/HomeScreen';
 import { Container } from 'react-bootstrap';
 
 const App = () => {
   return (
     <>
-      <Header></Header>
+      <Header />
       <main className='py-3'>
         <Container>
-          <h1>Welcome to ProShop</h1>
+          <HomeScreen />
         </Container>
       </main>
-      <Footer></Footer>
+      <Footer />
     </>
   );
 };
