be694f6f49294a8bd79b15a55f393f398bbeaca4
Jade <hendricksjade@gmail.com>
2022-07-24 16:23:59 +0200

    adding in react-bootstrap, setting up the inital header, main and footer components.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from "./components/Footer";
+import { Container } from 'react-bootstrap';
+
+const App = () => {
+  return (
+    <>
+      <Header></Header>
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to ProShop</h1>
+        </Container>
+      </main>
+      <Footer></Footer>
+    </>
+  );
+};
+
+export default App;
