600c4d9530debc6acd0e75c39cb671b2e6271aad
Jade <hendricksjade@gmail.com>
2022-07-24 17:26:46 +0200

    implementing react-router into the application.

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types'
 import Rating from '../components/Rating';
 
 const Product = ({product}) => {
   return (
     <Card className='my-3 p-3 rounded'>
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
             <Card.Img src={product.image} variant='top' />
-        </a>
+        </Link>
         <Card.Body>
-            <a href={`/product/${product._id}`}>
+            <Link to={`/product/${product._id}`}>
                 <Card.Title as='div'>
                     <strong>{product.name}</strong>
                 </Card.Title>
-            </a>
+            </Link>
             <Card.Text as='div'>
                 <div className='my-3'>
                     <Rating value={product.rating} text={`${product.numReviews} reviews`} />
@@ -37,4 +38,4 @@ Rating.propTypes = {
     color: PropTypes.string
 }
 
-export default Product
+export default Product;
