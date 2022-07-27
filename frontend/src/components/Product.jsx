928be9166c6d6d75e05ec45134c0f9650a3a7450
Jade <hendricksjade@gmail.com>
2022-07-27 17:48:53 +0200

    fixing router issues and fixing proptype undefined error.

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,41 +1,34 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types'
 import Rating from '../components/Rating';
 
-const Product = ({product}) => {
-  return (
-    <Card className='my-3 p-3 rounded'>
-        <Link to={`/product/${product._id}`}>
-            <Card.Img src={product.image} variant='top' />
-        </Link>
-        <Card.Body>
+const Product = ({ product }) => {
+    return (
+        <Card className='my-3 p-3 rounded'>
             <Link to={`/product/${product._id}`}>
-                <Card.Title as='div'>
-                    <strong>{product.name}</strong>
-                </Card.Title>
+                <Card.Img src={product.image} variant='top' />
             </Link>
-            <Card.Text as='div'>
-                <div className='my-3'>
-                    <Rating value={product.rating} text={`${product.numReviews} reviews`} />
-                </div>
-            </Card.Text>
-            <Card.Text as='h3'>{product.price}</Card.Text>
-        </Card.Body>
+            <Card.Body>
+                <Link to={`/product/${product._id}`}>
+                    <Card.Title as='div'>
+                        <strong>{product.name}</strong>
+                    </Card.Title>
+                </Link>
+                <Card.Text as='div'>
+                    <div className='my-3'>
+                        <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+                    </div>
+                </Card.Text>
+                <Card.Text as='h3'>{product.price}</Card.Text>
+            </Card.Body>
 
-    </Card>
-  )
+        </Card>
+    )
 }
 
 Rating.defaultProps = {
     color: '#f8e825'
-}
-
-Rating.propTypes = {
-    value: PropTypes.number.isRequired,
-    text: PropTypes.string.isRequired,
-    color: PropTypes.string
 }
 
 export default Product;
