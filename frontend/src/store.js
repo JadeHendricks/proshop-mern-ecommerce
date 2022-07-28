a493ee21cf6805b3277505bf093a4737bbca5928
Jade <hendricksjade@gmail.com>
2022-07-28 21:00:35 +0200

    adding in the product list reducer and action files.

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,9 +2,14 @@ import { createStore, combineReducers, applyMiddleware }  from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
+import { productListReducer } from './reducers/ProductReducers';
+
 const initialState = {};
 
-const reducer = combineReducers({});
+const reducer = combineReducers({
+    productList: productListReducer
+});
+
 const middleware = [thunk];
 const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)));
 
