0e72e47e68f1153338b9048313ddbfaaef7f8486
Jade <hendricksjade@gmail.com>
2022-07-31 18:10:36 +0200

    adding in the cart reducer and add to cart button.

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,13 +2,21 @@ import { createStore, combineReducers, applyMiddleware }  from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-import { productListReducer, productDetailsReducer } from './reducers/ProductReducers';
+import { productListReducer, productDetailsReducer } from './reducers/productReducers';
+import { cartReducer } from './reducers/cartReducers';
 
-const initialState = {};
+const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [];
+
+const initialState = {
+    cart: {
+        cartItems: cartItemsFromStorage
+    }
+};
 
 const reducer = combineReducers({
     productList: productListReducer,
-    productDetails: productDetailsReducer
+    productDetails: productDetailsReducer,
+    cart: cartReducer
 });
 
 const middleware = [thunk];
