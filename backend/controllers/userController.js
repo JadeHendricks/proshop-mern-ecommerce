f894ec1fb0aa892452f29d829027796edd32c656
Jade <hendricksjade@gmail.com>
2022-08-02 16:49:32 +0200

    adding in the user registration functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -20,7 +20,32 @@ const authUser = asyncHandler(async(req, res) => {
     }
 });
 
-const getUserProfule = asyncHandler(async(req, res) => {
+const registerUser = asyncHandler(async(req, res) => {
+    const { name, email, password } = req.body;
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+        res.status(400);
+        throw new Error('User Already Exsists');
+    }
+
+    const user = await User.create({name, email, password});
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id) 
+        });
+    } else {
+        res.status(400);
+        throw new Error('Invalid user data');
+    }
+});
+
+const getUserProfile = asyncHandler(async(req, res) => {
     const user = await User.findById(req.user._id);
     if (user) {
         res.json({
@@ -36,4 +61,4 @@ const getUserProfule = asyncHandler(async(req, res) => {
     res.send('Success');
 });
 
-export { authUser, getUserProfule };
+export { authUser, getUserProfile, registerUser };
